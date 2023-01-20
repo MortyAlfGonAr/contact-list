2621d5137476e3527868c6ff825e708c66c3c7da
Martin Gonzalez <martin.a.g.a@hotmail.com>
2023-01-19 21:33:30 -0500

    Actualizada rama project-1

--- contactos.js
+++ contactos.js
@@ -1,21 +1,17 @@
-// 1. Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
-let listaContactos = ['Graciela González', 'Cristian Martínez', 'Juan Tula', 'Otto Arrieta'];
+let listaContactos = ['Sherlock Holmes', 'Rick Sanchez', 'Homero Simpson'];
 
-// 2. Crea una función para añadir un nuevo contacto a una lista
 function agregarContacto(nombreContacto){
     listaContactos.push(nombreContacto);
 }
 
-// 3. Crea una función para borrar un contacto existente de la lista
 function borrarContacto(nombreContacto){
     let indiceContacto = listaContactos.indexOf(nombreContacto);
     listaContactos.splice(indiceContacto, 1);
 }
-// 4. Crea una función para imprimir en consola los contactos presentes en la lista
-// Usando el método sort() los ordeno alfabéticamente: 
 
 function mostrarContactos(){
-    for (const contacto of listaContactos.sort()) {
+    listaContactos.sort();
+    for (const contacto of listaContactos) {
         console.log(contacto);
     }
 }
